0948b038bbcdd9a12bae697d6c9363aafa9a492b
OlekUkraine <olekfromukraine@gmail.com>
2023-07-26 14:19:51 +0300

    node hw-__ ts

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,9 +1,9 @@
-import { config } from "dotenv";
+import { config} from "dotenv";
 
 config();
 
 export const configs = {
-  PORT: process.env.PORT,
+      PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
 
   FRONT_URL: process.env.FRONT_URL,
