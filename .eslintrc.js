de5775a556cd08d92ebba899c412a6f2d27b4522
OlekUkraine <olekfromukraine@gmail.com>
2023-07-26 23:29:11 +0300

    ESlint __ fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,11 +43,3 @@ module.exports = {
     },
     ignorePatterns: ['.eslintrc.js']
 };
-
-/* eslint-env node */
-// module.exports = {
-//     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-//     parser: '@typescript-eslint/parser',
-//     plugins: ['@typescript-eslint'],
-//     root: true,
-// };
